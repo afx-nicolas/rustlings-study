d90438bee0257c228626a22d657429defd26b1cd
Nicolas A. Feitoza <afxnicolas@gmail.com>
2025-07-13 13:46:19 -0300

    feat(errors): solve `errors2.rs`

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -16,14 +16,14 @@
 
 use std::num::ParseIntError;
 
+const PROCESSING_FEE: i32 = 1;
+const COST_PER_ITEM: i32 = 5;
+
 fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
-    let processing_fee = 1;
-    let cost_per_item = 5;
 
-    // TODO: Handle the error case as described above.
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?;
 
-    Ok(qty * cost_per_item + processing_fee)
+    Ok(qty * COST_PER_ITEM + PROCESSING_FEE)
 }
 
 fn main() {
